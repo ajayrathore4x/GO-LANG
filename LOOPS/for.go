a40c69ff1ba086281af50f04139240f50a6cf7ad
Ajay Rathore <ajayx0094@gmaol.com>
2026-09-15 12:03:44 +0000

    idefinite loop and range loop

--- LOOPS/for.go
+++ LOOPS/for.go
@@ -3,14 +3,26 @@ package main
 import "fmt"
 
 func main() {
-	for i := 0; i < 5; i++ {
-		fmt.Println(i)
-	}
+	// for i := 0; i < 5; i++ {
+	// 	fmt.Println(i)
+	// }
 
 	// Another way to write a for loop like a while loop
-	i := 0
-	for i < 5 {
-		fmt.Println(i)
-		i++
-	}
+	// i := 0
+	// for i < 5 {
+	// 	fmt.Println(i)
+	// 	i++
+	// }
+
+	// for indefinite loop
+	//j := 0
+	// for {
+	// 	fmt.Println(j)
+	// 	j++
+	// }
+
+	// for range loop
+	// for i:= range 3 {
+	// 	fmt.Println(i)
+	// }
 }
