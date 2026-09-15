6774dabb8212354a4420ad58b2a0bb1420fa208a
Ajay Rathore <ajayx0094@gmaol.com>
2026-09-15 02:50:05 +0000

    variables and constants

--- main.go
+++ main.go
@@ -3,5 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello world")
+	var name string 
+    age := 21
+    var cgpa float64 = 7.2
+    var isStudent bool = true
+
+	fmt.Print("Enter your name: ")
+    fmt.Scanln(&name)
+    fmt.Println(name)
+    fmt.Println(age)
+    fmt.Println(cgpa)
+    fmt.Println(isStudent)
+
+	fmt.Printf("My name is %s, I am %d years old, my CGPA is %.2f and it is %t that I am a student.\n", name, age, cgpa, isStudent)
+
 }
