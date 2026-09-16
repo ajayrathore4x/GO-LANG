ae6c51978dd5db7a1b1805de4e3e87a7ebfa024f
Ajay Rathore <ajayx0094@gmail.com>
2026-09-16 01:41:17 +0000

    if else / break - continue

--- IF-ELSE/condition.go
+++ IF-ELSE/condition.go
@@ -14,4 +14,34 @@ func main() {
 		fmt.Println("you're not eligible to vote ")
 	}
 
+	var marks int
+
+	fmt.Print("Enter your marks: ")
+	fmt.Scanln(&marks)
+
+	if marks >= 90 {
+		fmt.Println("A")
+	} else if marks >= 75 {
+		fmt.Println("B")
+	} else if marks >= 50 {
+		fmt.Println("C")
+	} else {
+		fmt.Println("Fail")
+	}
+
+	// break
+	for i := 1; i <= 10; i++ {
+    if i == 5 {
+        break
+    }
+    fmt.Println(i)
+    }
+
+	//continue
+	for i := 1; i <= 5; i++ {
+    if i == 3 {
+        continue
+    }
+    fmt.Println(i)
+    }
 }
