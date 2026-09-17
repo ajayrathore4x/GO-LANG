196c83e29ef5df2f2fc715823e8ff31eb23acfa1
Ajay Rathore <ajayx0094@gmail.com>
2026-09-17 04:30:21 +0000

    function return multiple values

--- FUNCTIONS/func.go
+++ FUNCTIONS/func.go
@@ -10,7 +10,17 @@ func add(a int, b int) int {
     return (a + b)
 }
 
+func calculate(a, b int) (int, int) {
+    sum := a + b
+    difference := a - b
+
+    return sum, difference
+}
 func main(){
 	greet()
 	fmt.Println(add(7,7))
+	sum, difference := calculate(10, 3)
+
+	fmt.Println(sum)
+	fmt.Println(difference)
 }
