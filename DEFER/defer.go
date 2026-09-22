bd0a70de9833ca98e4b7c1df0309a982c03d8cb8
Ajay Rathore <ajayx0094@gmail.com>
2026-09-22 12:45:52 +0000

    multiple defer

--- DEFER/defer.go
+++ DEFER/defer.go
@@ -7,6 +7,9 @@ func main() {
 	fmt.Println("Start")
 
 	defer fmt.Println("This runs at the end")
+	defer fmt.Println("First")
+	defer fmt.Println("Second")
+	defer fmt.Println("Third")
 
 	fmt.Println("Middle")
 }
